dde82796721c7ecfb64362b6f637cf86c7f79028
Kush <kushpatel124@gmail.com>
2023-02-18 16:24:17 -0500

    commit tree 6df4c85506a1a3e0fe230c4ada1479943b14bf17

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import Header from "./Header";
 import Profile from "./Profile";
 
+// import the provider
+import { UserProvider } from "../context/user";
+
 function App() {
   const [theme, setTheme] = useState("dark");
   const [user, setUser] = useState(null);
